b23671898639335e42408896b436cac404dceae0
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-13 22:01:28 +0530

    Implemented AnswerService class with createAnswer functionality

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -0,0 +1,56 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.AnswerDao;
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserAuthDao;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Service
+public class AnswerService {
+    @Autowired
+    private UserAuthDao userAuthDao;
+
+    @Autowired
+    private AnswerDao answerDao;
+
+    @Autowired
+    private QuestionDao questionDao;
+    // @Transactional annotation is the metadata that specifies the semantics of the transactions on a method
+    // It is declarative way to rollback a transaction
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity createAnswer(AnswerEntity answerEntity, final String accessToken, final String questionId)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+        //check weather user is signed in or not.
+        if(userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if(userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException( "ATHR-002", "User is signed out.Sign in first to post an answer");
+        }
+        QuestionEntity questionEntity = questionDao.getQuestionByUuid(questionId);
+
+        //check whether the provided question is valid or not
+        if(questionEntity == null) {
+            throw new InvalidQuestionException("QUES-001", "The question entered is invalid");
+        }
+        // If none of the exceptions above are thrown, means user is signed in and question is valid
+        // set the attributes of answerEntity and call createAnswer method from AnswerDao
+        answerEntity.setUuid(UUID.randomUUID().toString());
+        answerEntity.setDate(ZonedDateTime.now());
+        answerEntity.setQuestionEntity(questionEntity);
+        answerEntity.setUserEntity(userAuthEntity.getUserEntity());
+        return answerDao.createAnswer(answerEntity);
+    }
+
+}
