da8b9b4342eceb6a4af00d7b7aaf6b4e852a9888
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-13 20:22:31 +0530

    Implemented AnswerDao class
    Implemented AnswerDao class for performing CRUD operations on answer table in the database.

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,20 @@
+package com.upgrad.quora.service.dao;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+//Dao class for performing CRUD operations on answer table in the database.
+@Repository
+public class AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //This method persists the AnswerEntity in the database
+    public AnswerEntity createAnswer(AnswerEntity answerEntity) {
+        entityManager.persist(answerEntity);
+        return answerEntity;
+    }
+}
