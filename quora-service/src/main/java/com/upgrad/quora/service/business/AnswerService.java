80849c62355d7ae5392e6f6195202de34901b19a
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-16 15:25:06 +0530

    Added editAnswerContent functionality in AnswerService

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -6,6 +6,7 @@ import com.upgrad.quora.service.dao.UserAuthDao;
 import com.upgrad.quora.service.entity.AnswerEntity;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,5 +55,31 @@ public class AnswerService {
         answerEntity.setUserEntity(userAuthEntity.getUserEntity());
         return answerDao.createAnswer(answerEntity);
     }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity editAnswerContent(final String accessToken, final String answerId, final String newAnswer)
+            throws AnswerNotFoundException, AuthorizationFailedException {
 
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+        //check whether user is signed in or not.If not then it will throw an exception
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to edit an answer");
+        }
+        // check whether the answer exist or not
+        AnswerEntity answerEntity = answerDao.getAnswerById(answerId);
+        if (answerEntity == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+        //check editing user is owner of the answer or not
+        if (!answerEntity.getUserEntity().getUuid().equals(userAuthEntity.getUserEntity().getUuid())) {
+            throw new AuthorizationFailedException(
+                    "ATHR-003", "Only the answer owner can edit the answer");
+        }
+        //Now we will update new edited answer
+        answerEntity.setAnswer(newAnswer);
+        answerDao.updateAnswer(answerEntity);
+        return answerEntity;
+    }
 }
