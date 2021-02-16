d9720d293f7d86d56023bd7f4ec95db2e1d8f41b
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-16 15:12:19 +0530

    Added getAnswerById functionality

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -3,6 +3,7 @@ import com.upgrad.quora.service.entity.AnswerEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 //Dao class for performing CRUD operations on answer table in the database.
@@ -17,5 +18,13 @@ public class AnswerDao {
     public AnswerEntity createAnswer(AnswerEntity answerEntity) {
         entityManager.persist(answerEntity);
         return answerEntity;
+    }
+    // Method to fetch answerEntity from database as per their Id
+    public AnswerEntity getAnswerById(final String answerId) {
+        try {
+            return entityManager.createNamedQuery("getAnswerByUuid", AnswerEntity.class).setParameter("uuid", answerId).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 }
