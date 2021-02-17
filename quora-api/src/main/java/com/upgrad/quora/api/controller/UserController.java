4d7769f332637b2e09cd2aae6f5e883c30ce7f05
hellosibun <sibun.soumya@gmail.com>
2021-02-17 17:11:48 +0530

    added usersignout method

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.upgrad.quora.api.controller;
 
 
+import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignoutResponse;
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.UserAuthenticationBusinessService;
 import com.upgrad.quora.service.business.UserBusinessService;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import java.util.Base64;
 import java.util.UUID;
@@ -77,6 +81,27 @@ public class UserController {
         // The Http Status code 200 , the response code HttpStatus.OK and the status message SIGNED IN SUCCESSFULLY
 
         return new ResponseEntity<SigninResponse>(authenticatedSigninResponse, headers, HttpStatus.OK);
+    }
+
+    //**userSignout**//
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignoutResponse> userSignout(@RequestHeader("authorization") final String authorization)
+            throws SignOutRestrictedException {
+        //The input can be of any form "Bearer <accesstoken>" or "<accesstoken>" in the authorization header
+
+        UserAuthTokenEntity userAuthToken;
+        try {
+            String[] bearerAccessToken = authorization.split("Bearer ");
+            userAuthToken = userAuthenticationBusinessService.signOut(bearerAccessToken[1]);
+        } catch (ArrayIndexOutOfBoundsException are) {
+            userAuthToken = userAuthenticationBusinessService.signOut(authorization);
+        }
+        UserEntity user = userAuthToken.getUser();
+
+        SignoutResponse authorizedSignoutResponse = new SignoutResponse().id(user.getUuid()).message("SIGNED OUT SUCCESSFULLY");
+        //This method returns an object of SignoutResponse and HttpStatus
+        return new ResponseEntity<SignoutResponse>(authorizedSignoutResponse, HttpStatus.OK);
     }
 
 
