fac758257e3b77d6692b1f92bb7aa9ce0756e98f
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-19 16:55:38 +0530

    Added deleteAnswer functionality in AnswerService class

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -56,6 +56,8 @@ public class AnswerService {
         return answerDao.createAnswer(answerEntity);
     }
     @Transactional(propagation = Propagation.REQUIRED)
+    // This service method is used to edit answer by calling another method from AnswerDao class
+    // This method accepts answerID, accessToken and new edited answer in string format as parameter
     public AnswerEntity editAnswerContent(final String accessToken, final String answerId, final String newAnswer)
             throws AnswerNotFoundException, AuthorizationFailedException {
 
@@ -81,5 +83,34 @@ public class AnswerService {
         answerEntity.setAnswer(newAnswer);
         answerDao.updateAnswer(answerEntity);
         return answerEntity;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    // This service method is used to delete answer by calling another method from AnswerDao class
+    // This method accepts answerID and accessToken in string format as parameter
+    public AnswerEntity deleteAnswer(final String answerId, final String accessToken)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+        //It will check weather user is signed in or not.If not then it will throw an exception
+
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to delete an answer");
+        }
+        //It will check the answer exist or not
+        AnswerEntity answerEntity = answerDao.getAnswerById(answerId);
+        if (answerEntity == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+        //it will check deleting user is owner of the answer or admin or not
+        if (userAuthEntity.getUserEntity().getRole().equals("admin") || answerEntity.getUserEntity().getUuid().equals(userAuthEntity.getUserEntity().getUuid())) {
+            return answerDao.deleteAnswer(answerId);
+        } else {
+            throw new AuthorizationFailedException(
+                    "ATHR-003", "Only the answer owner or admin can delete the answer");
+        }
     }
 }
