f5e75950fc14dd2ba6114f171fda98aff9ec6f94
Swayambhu872 <67875350+Swayambhu872@users.noreply.github.com>
2021-02-21 12:55:54 +0530

    Added getAllAnswersToQuestion functionality in AnswerService class

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 //@Service annotation is used to mark a class as a service provider
 @Service
@@ -112,5 +113,28 @@ public class AnswerService {
             throw new AuthorizationFailedException(
                     "ATHR-003", "Only the answer owner or admin can delete the answer");
         }
+    }
+    
+    //Service Method to get all answer for question
+    // Accepts questionId, accessToken as a parameter and returns alist of answers
+    public List<AnswerEntity> getAllAnswersToQuestion(
+            final String questionId, final String accessToken)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthTokenEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+        //It will check weather user is signed in or not.If not then it will throw an exception
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to get the answers");
+        }
+        //It will check weather question exist or not
+        QuestionEntity questionEntity = questionDao.getQuestionByUuid(questionId);
+        if (questionEntity == null) {
+            throw new InvalidQuestionException(
+                    "QUES-001", "The question with entered uuid whose details are to be seen does not exist");
+        }
+        //return all answer of given question
+        return answerDao.getAllAnswersToQuestion(questionId);
     }
 }
